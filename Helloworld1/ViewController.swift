7609fae7063a3d791e58521d4b2ee857ca0a1a11
taiseitakai <taisei-0629-moco@ezweb.ne.jp>
2020-10-26 23:55:12 +0900

    Initial Commit

---
+++ Helloworld1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Helloworld1
+//
+//  Created by 高井大政 on 2020/10/26.
+//  Copyright © 2020 taisei.takai1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
